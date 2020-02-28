72a5e29b438100b0cacf78322edaa445bc231baf
Kwong <wah.wai.kwong@accenture.com>
2020-02-28 16:52:04 +0800

    test server

---
+++ Mission6-SpringServer/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+ù
+mMission6-SpringServer/src/main/java/com/DevTraining/Mission6SpringServer/Mission6SpringServerApplication.java,2\8\2860722bea91a63653b19575771f5c43b4246ac1
---
+++ Mission6-SpringServer/Mission6-SpringServer/src/test/java/com/DevTraining/Mission6SpringServer/Mission6SpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevTraining.Mission6SpringServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mission6SpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
